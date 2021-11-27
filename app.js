bfd77c87c606ad204e3ce768126988cefeb17b4b
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-11-27 00:19:57 -0800

    Add id 1

---
+++ app.js
@@ -0,0 +1,12 @@
+const reviews = [
+    {
+      id: 1,
+      name: "Muhammad Salman",
+      job: "Web and Mobile Application Developer",
+      img:
+        "https://i2.wp.com/allovershayari.com/wp-content/uploads/2021/06/Boy-Dp-Sad-AOS.jpg?resize=577%2C600&ssl=1",
+      text:
+        "My name is M. Salman. My Father name is Tariq Nazir. My Qualification is BS Computer Sciencs (Continue). I live in Faisalabad.",
+    },
+    
+]
